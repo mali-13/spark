6ef0c9dff2ccff0ecf57474bab4f0c5a6fef99a2
mali <hello@xhemaj.dev>
2024-01-09 16:06:50 +0100

    feat: Filter and count from text file

--- lesson-1-streaming-dataframes/exercises/starter/hellospark.py
+++ lesson-1-streaming-dataframes/exercises/starter/hellospark.py
@@ -1,28 +1,28 @@
 from pyspark.sql import SparkSession
 
-# TO-DO: create a variable with the absolute path to the text file
-# /home/workspace/lesson-1-streaming-dataframes/exercises/starter/Test.txt
+# Create a variable with the absolute path to the text file
+text = "/home/workspace/lesson-1-streaming-dataframes/exercises/starter/Test.txt"
 
-# TO-DO: create a Spark session
+# Create a Spark session
+sparkSession = SparkSession.builder.appName("HelloSpark").getOrCreate()
 
-# TO-DO: set the log level to WARN
+# Set the log level to WARN
+sparkSession.sparkContext.setLogLevel('WARN')
 
-# TO-DO: using the Spark session variable, call the appropriate
-# function referencing the text file path to read the text file 
-
-# TO-DO: create a global variable for number of times the letter a is found
-# TO-DO: create a global variable for number of times the letter b is found
-
-# TO-DO: create a function which accepts a row from a dataframe, which has a column called value
-# in the function increment the a count variable for each occurrence of the letter a
-# in the value column
-
-# TO-DO: create another function which accepts a row from a dataframe, which has a column called value
-# in the function increment the b count variable for each occurrence of the letter b
-# in the value column
+# Using the Spark session variable, call the appropriate
+# function referencing the text file path to read the text file
+textData = sparkSession.read.text(text).cache()
 
 
-# TO-DO: use the forEach method to invoke the a counting method
-# TO-DO: use the forEach method to invoke the b counting method
+# Create a global variable for number of times the letter a is found
+numAs = textData.filter(textData.value.contains('a')).count()
+# Create a global variable for number of times the letter b is found
+numBs = textData.filter(textData.value.contains('b')).count()
 
-# TO-DO: stop the spark application
+print("*******")
+print("*******")
+print("*****Lines with a: %i, lines with b: %i" % (numAs, numBs))
+print("*******")
+print("*******")
+
+sparkSession.stop()
