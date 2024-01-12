26b7946007b6618af050a27ad3e5b192f660c06c
mali <hello@xhemaj.dev>
2024-01-12 16:37:33 +0100

    feat: Read json kafka and write to console

--- lesson-2-joins-and-json/exercises/starter/bank-deposits.py
+++ lesson-2-joins-and-json/exercises/starter/bank-deposits.py
@@ -1,30 +1,57 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, to_json, col, unbase64, base64, split, expr
-from pyspark.sql.types import StructField, StructType, StringType, BooleanType, ArrayType, DateType
+from pyspark.sql.types import (
+    StructField,
+    StructType,
+    StringType,
+    DoubleType
+)
 
-# TO-DO: create a kafka message schema StructType including the following JSON elements:
+# Create a kafka message schema StructType including the following JSON elements:
 # {"accountNumber":"703934969","amount":415.94,"dateAndTime":"Sep 29, 2020, 10:06:23 AM"}
+depositSchema = StructType(
+    [
+        StructField("accountNumber", StringType()),
+        StructField("amount", DoubleType()),
+        StructField("dateAndTime", StringType()),
+    ]
+)
 
-# TO-DO: create a spark session, with an appropriately named application name
+# Create a spark session, with an appropriately named application name
+sparkSession = SparkSession.builder.appName("bank-deposits").getOrCreate()
+sparkSession.sparkContext.setLogLevel("WARN")
 
-#TO-DO: set the log level to WARN
+# Read the bank-deposits kafka topic as a source into a streaming dataframe with the bootstrap server kafka:19092,
+# configuring the stream to read the earliest messages possible
+bankDepositsRawStreamingDF = (
+    sparkSession.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", "kafka:19092")
+    .option("subscribe", "bank-deposits")
+    .option("startingOffsets", "earliest")
+    .load()
+)
 
-#TO-DO: read the atm-visits kafka topic as a source into a streaming dataframe with the bootstrap server kafka:19092, configuring the stream to read the earliest messages possible                                    
+# Using a select expression on the streaming dataframe, cast the key and the value columns from kafka as strings,
+# and then select them
+bankDepositsStreamingDF = bankDepositsRawStreamingDF.selectExpr(
+    "cast(key as string) key", "cast(value as string) value"
+)
 
-#TO-DO: using a select expression on the streaming dataframe, cast the key and the value columns from kafka as strings, and then select them
+# Using the kafka message StructType, deserialize the JSON from the streaming dataframe
 
-#TO-DO: using the kafka message StructType, deserialize the JSON from the streaming dataframe 
-
-# TO-DO: create a temporary streaming view called "BankDeposits" 
+# Create a temporary streaming view called "BankDeposits"
 # it can later be queried with spark.sql
+bankDepositsStreamingDF.withColumn("value", from_json("value", depositSchema)).select(
+    col("value.*")
+).createOrReplaceTempView("BankDeposits")
 
-#TO-DO: using spark.sql, select * from BankDeposits
+# Using spark.sql, select * from BankDeposits
+bankDepositsSelectDF = sparkSession.sql("select * from BankDeposits")
 
-# TO-DO: write the stream to the console, and configure it to run indefinitely, the console output will look something like this:
+# Write the stream to the console, and configure it to run indefinitely, the console output will look something like
+# this: +-------------+------+--------------------+ |accountNumber|amount|         dateAndTime|
+# +-------------+------+--------------------+ |    103397629| 800.8|Oct 6, 2020 1:27:...|
 # +-------------+------+--------------------+
-# |accountNumber|amount|         dateAndTime|
-# +-------------+------+--------------------+
-# |    103397629| 800.8|Oct 6, 2020 1:27:...|
-# +-------------+------+--------------------+
-
-
+bankDepositsSelectDF.writeStream.outputMode("append").format(
+    "console"
+).start().awaitTermination()
