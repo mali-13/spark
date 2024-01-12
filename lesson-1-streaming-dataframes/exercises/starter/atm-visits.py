89db2c3546e20bc06b495b6f38e65f79d7af8405
mali <hello@xhemaj.dev>
2024-01-12 15:04:47 +0100

    feat: Read from kafka, cast key and value to string

--- lesson-1-streaming-dataframes/exercises/starter/atm-visits.py
+++ lesson-1-streaming-dataframes/exercises/starter/atm-visits.py
@@ -1,16 +1,40 @@
 from pyspark.sql import SparkSession
 
-# TO-DO: create a spark session, with an appropriately named application name
+# Create a spark session, with an appropriately named application name
+sparkSession = SparkSession.builder.appName("atm-visits").getOrCreate()
 
-#TO-DO: set the log level to WARN
+# Set the log level to WARN
+sparkSession.sparkContext.setLogLevel("WARN")
 
-#TO-DO: read the atm-visits kafka topic as a source into a streaming dataframe with the bootstrap server kafka:19092, configuring the stream to read the earliest messages possible                                    
+# Read the atm-visits kafka topic as a source into a streaming dataframe with the bootstrap server kafka:19092,
+# configuring the stream to read the earliest messages possible
+atmVisitsRawStreamingDF = (
+    sparkSession.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", "kafka:19092")
+    .option("subscribe", "atm-visits")
+    .option("startingOffsets", "earliest")
+    .load()
+)
 
-#TO-DO: using a select expression on the streaming dataframe, cast the key and the value columns from kafka as strings, and then select them
+# Using a select expression on the streaming dataframe, cast the key and the value columns from kafka as strings,
+# and then select them
+atmVisitsStreamingDF = atmVisitsRawStreamingDF.selectExpr(
+    "cast(key as string) transactionId", "cast(value as string) location"
+)
 
-# TO-DO: create a temporary streaming view called "ATMVisits" based on the streaming dataframe
+# Create a temporary streaming view called "ATMVisits" based on the streaming dataframe
+atmVisitsStreamingDF.createOrReplaceTempView("ATMVisits")
 
-# TO-DO query the temporary view with spark.sql, with this query: "select * from ATMVisits"
+# Query the temporary view with spark.sql, with this query: "select * from ATMVisits"
+atmVisitsSelectStarDF = sparkSession.sql("select * from ATMVisits")
 
-# TO-DO: write the dataFrame from the last select statement to kafka to the atm-visit-updates topic, on the broker kafka:19092
-# TO-DO: for the "checkpointLocation" option in the writeStream, be sure to use a unique file path to avoid conflicts with other spark scripts
+# Write the dataFrame from the last select statement to kafka to the atm-visit-updates topic, on the broker
+# kafka:19092 for the "checkpointLocation" option in the writeStream, be sure to use a unique file path to avoid
+# conflicts with other spark scripts
+atmVisitsSelectStarDF.selectExpr(
+    "cast(transactionId as string) as key", "cast(location as string) as value"
+).writeStream.format("kafka").option("kafka.bootstrap.servers", "kafka:19092").option(
+    "topic", "atm-visit-updates"
+).option(
+    "checkpointLocation", "/tmp/kafkacheckpoint_1"
+).start().awaitTermination()
